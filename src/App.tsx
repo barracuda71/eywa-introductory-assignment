4feef000d33cd24c683ba3dd80948a4614783497
chrd <chrd@bk.ru>
2021-09-29 20:57:12 +0300

    add emotion.js and use it;

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Faucet} from "./faucet/Faucet";
 
 const App = () =>
     (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Read README.md to start</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <Faucet/>
             </header>
         </div>
     );
